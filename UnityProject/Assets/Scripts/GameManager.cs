744532c70766bac29d63aac6cde1badc359cc08a
KDH <eogh819@naver.com>
2020-10-17 20:48:06 +0900

    메니저 스크립트 작성중 해상도 조절 코드 테스트

---
+++ UnityProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Android;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{  
+    /// <summary>
+    /// 싱글턴 인스턴트
+    /// </summary>
+    protected static GameManager instance;
+    public static GameManager Instance { get { return instance; } }
+    //-------------------------------------------------------------//
+
+    /// <summary>
+    /// 악마, 촉수, 저주, 마물, 언데드, 자연, 천사
+    /// </summary>
+    ulong ulong_resource_evil, ulong_resource_teil, ulong_resource_curse, ulong_resource_monster, 
+        ulong_resource_undead, ulong_resource_nature, ulong_resource_angle;
+
+    /// <summary>
+    /// 플레이어 레벨
+    /// </summary>
+    ushort ushort_maxLevel, ushort_curLevel;
+
+    private void Awake()
+    {
+        //싱글턴 인스턴트 생성------
+        if (instance)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+        instance = this;
+        //--------------------------
+
+        //제거 방지
+        DontDestroyOnLoad(gameObject);
+    }
+
+    /// <summary>
+    /// 초기화
+    /// </summary>
+    private void Init()
+    {
+        ulong_resource_evil = 0;
+        ulong_resource_teil = 0;
+        ulong_resource_curse = 0;
+        ulong_resource_monster = 0;
+        ulong_resource_undead = 0;
+        ulong_resource_nature = 0;
+        ulong_resource_angle = 0;
+
+        ushort_maxLevel = 10;
+        ushort_curLevel = 1;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
--- UnityProject/Assets/Scripts/SceneManager_Script.cs
+++ UnityProject/Assets/Scripts/SceneManager_Script.cs
@@ -5,11 +5,58 @@ public class SceneManager_Script : MonoBehaviour
 {
     public string str_target_sceneName;
 
+    private void Start()
+    {
+       //// Screen.SetResolution(1440, 2560, false);
+        RescaleCamera();
+    }
+
     /// <summary>
     /// test code , 1번씬 불러옴
     /// </summary>
     public void SceneNext()
     {       
         SceneManager.LoadScene(str_target_sceneName);
+    }
+
+    private int ScreenSizeX = 0;
+    private int ScreenSizeY = 0;
+    private void RescaleCamera()
+    {
+
+        if (Screen.width == ScreenSizeX && Screen.height == ScreenSizeY) return;
+
+        float targetaspect = 9f / 16.0f;
+        float windowaspect = (float)Screen.width / (float)Screen.height;
+        float scaleheight = windowaspect / targetaspect;
+        Camera camera = Camera.main;// GetComponent<Camera>();
+
+        if (scaleheight < 1.0f)
+        {
+            Rect rect = camera.rect;
+
+            rect.width = 1.0f;
+            rect.height = scaleheight;
+            rect.x = 0;
+            rect.y = (1.0f - scaleheight) / 2.0f;
+
+            camera.rect = rect;
+        }
+        else // add pillarbox
+        {
+            float scalewidth = 1.0f / scaleheight;
+
+            Rect rect = camera.rect;
+
+            rect.width = scalewidth;
+            rect.height = 1.0f;
+            rect.x = (1.0f - scalewidth) / 2.0f;
+            rect.y = 0;
+
+            camera.rect = rect;
+        }
+
+        ScreenSizeX = Screen.width;
+        ScreenSizeY = Screen.height;
     }
 }
